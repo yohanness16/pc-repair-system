81bf16c1e5081ef77f4265128a87c6baa8b14d17
yohannes desalegn <yohanness1621@gmail.com>
2025-08-28 23:08:52 +0300

    Yohannes (#7)
    * swagger
    
    * templets added

--- backend/Repairs/urls.py
+++ backend/Repairs/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import RepairRequestCreateView, RepairApprovalView ,CompleteRepairView , PartDetailView, PartListCreateView ,EquipmentRepairHistoryView , AdminRepairStatsView
+from .views import RepairRequestCreateView, RepairApprovalView ,CompleteRepairView , PartDetailView, PartListCreateView ,EquipmentRepairHistoryView , AdminRepairStatsView , EquipmentRepairPDFView , RepairReceiptPDFView
 
 urlpatterns = [
     path('request/', RepairRequestCreateView.as_view(), name='repair-request'),
@@ -10,5 +10,9 @@ urlpatterns = [
     path('parts/', PartListCreateView.as_view(), name='part-list-create'),
     path('repair-history/', EquipmentRepairHistoryView.as_view(), name='equipment-repair-history'),
     path('admin/stats/', AdminRepairStatsView.as_view(), name='admin-stats'),
+    path('equipment/<int:equipment_id>/repairs/pdf/', EquipmentRepairPDFView.as_view(), name='equipment-repair-pdf'),
+    path('repair/<int:repair_id>/receipt/pdf/', RepairReceiptPDFView.as_view(), name='repair-receipt-pdf'),
+
+
 
 ]
