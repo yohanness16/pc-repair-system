5002f3e9573acea5f3ba29c3ecbfc3326dce87ca
yohanness16 <yohanness1621@gmail.com>
2025-09-08 00:48:57 +0300

    deployment ready backend added waiterss

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -154,5 +154,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"] 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
