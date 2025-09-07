24fc5512d3720156cfeed45d0ced3f6b50d95d43
yohanness16 <yohanness1621@gmail.com>
2025-09-07 21:08:17 +0300

    updated backend with routes

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -152,6 +152,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"] 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
