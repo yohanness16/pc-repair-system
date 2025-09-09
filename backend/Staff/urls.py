c0f34271a3c107bd9b74d69e52d85818f29365d9
yohanness16 <yohanness1621@gmail.com>
2025-09-09 11:22:44 +0300

    staff by id

--- backend/Staff/urls.py
+++ backend/Staff/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView ,LogoutView , UpdateProfileView , MyTokenObtainPairView , PingView, ForgotPasswordView , RequestPasswordResetView, VerifyResetCodeView , StaffListView , StaffRepairProgressView
+from .views import RegisterView ,LogoutView , UpdateProfileView , MyTokenObtainPairView , PingView, ForgotPasswordView , RequestPasswordResetView, VerifyResetCodeView , StaffListView , StaffRepairProgressView , GetStaffByIdView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
@@ -13,6 +13,7 @@ urlpatterns = [
     path('request-reset/', RequestPasswordResetView.as_view(), name='request-reset'),
     path('verify-reset/', VerifyResetCodeView.as_view(), name='verify-reset'),
     path('list/', StaffListView.as_view(), name='staff-list'),
+    path('list/<int:pk>/', GetStaffByIdView.as_view(), name='get-staff-by-id'),
     path('<int:staff_id>/repairs/', StaffRepairProgressView.as_view(), name='staff-repair-progress'),
 
 
